5ad4e66700515e6a66f147ee3461167969954b90
mateusms0407 <168552718+mateusms0407@users.noreply.github.com>
2024-06-10 21:28:59 -0300

    trabalho ib

---
+++ questao03.py
@@ -0,0 +1,2 @@
+Frase=input("digite uma frase: ")
+print(F"essa frase possui {len(Frase)} caracteres")
--- questao3.py
+++
@@ -1,2 +0,0 @@
-Frase=input("digite uma frase: ")
-print(F"essa frase possui {len(Frase)} caracteres")
