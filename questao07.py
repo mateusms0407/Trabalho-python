2b0e98b656ae65af1552f847e7af7099fdedf716
mateusms0407 <168552718+mateusms0407@users.noreply.github.com>
2024-06-11 21:36:33 -0300

    alteracao trabalho

--- questao07.py
+++ questao07.py
@@ -1,7 +1,7 @@
 nome=input("nome do aluno: ")
 diciplina=input("qual a diciplina: ")
-nota1=int(input("nota parcial: "))
-nota2=int(input("nota bimestral: "))
+nota1=float(input("nota parcial: "))
+nota2=float(input("nota bimestral: "))
 media=(nota1+nota2)/2
 if media>5:
     s=("aprovado")
