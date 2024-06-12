caecc370f30737f877e47283356638a4bf7bfd3c
mateusms0407 <168552718+mateusms0407@users.noreply.github.com>
2024-06-11 21:45:22 -0300

    alteracao trabalho

--- questao06.py
+++ questao06.py
@@ -1,6 +1,6 @@
 nome=input("digite seu nome: ")
 idade=int(input("qual sua idade: "))
 if idade>15:
-    print(f"{nome} vocé pode votar")
+    print(f"{nome} vocé pode emitir seu titulo de eleitor")
 else:
-    print(f"{nome} vocé não pode votar")
+    print(f"{nome} vocé não pode emitir seu titulo de eleitor")
